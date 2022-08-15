7bb09d2244950658eab1b387fa85cba0b9def881
anna soloveva <anyuta-angel22@yandex.ru>
2022-08-15 17:14:02 +0300

    added tatle new dok

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача4 Напишите программу, которая принемает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Input integer three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ed = ((number % 100)/10);
+
+Console.WriteLine($"Last digit is {ed}");
+
+
